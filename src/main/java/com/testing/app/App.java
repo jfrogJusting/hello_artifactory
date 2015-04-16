a30752f0c04a034753e4cf7f2afc2bdb517c8607
justing <justing@jfrog.com>
2015-04-16 14:27:55 -0700

    back to basics

--- src/main/java/com/testing/app/App.java
+++ src/main/java/com/testing/app/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, Try 2!" );
-		System.out.println( "Now with MORE TEXT!" );
-		System.out.println( "Now with even MORE TEXT!" );
+        System.out.println( "Hello World, Final!" );
     }
 }
