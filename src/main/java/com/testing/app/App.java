cf9e608fed4f845afca148318c38d13f9e065a5e
justing <justing@jfrog.com>
2015-04-16 14:33:01 -0700

    this should fail

--- src/main/java/com/testing/app/App.java
+++ src/main/java/com/testing/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, Finally!" );
+        System.out.println( "Hello World, Finally this should fail!" );
     }
 }
