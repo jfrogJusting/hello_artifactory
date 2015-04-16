8ec0d45a19f80673c5da385b14498716fd6e0846
justing <justing@jfrog.com>
2015-04-16 13:27:24 -0700

    OMG, another line of textgit pull

--- src/main/java/com/testing/app/App.java
+++ src/main/java/com/testing/app/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World, Try 2!" );
 		System.out.println( "Now with MORE TEXT!" );
+		System.out.println( "Now with even MORE TEXT!" );
     }
 }
